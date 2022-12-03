99b2be3addddbc98f6d4aa6fc716dc9ffcb77bed
tanmaykapoor <tanmay.skater@gmail.com>
2022-12-03 18:01:07 -0500

    remove body parser

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const userRoutes = require("./routes/user");
