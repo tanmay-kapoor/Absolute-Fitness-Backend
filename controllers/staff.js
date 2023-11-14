3a24b4bd40f4a0d975baedcf4753d13512f8d942
supra <rikc.2000@gmail.com>
2023-11-14 16:50:17 -0500

    minor changes

--- controllers/staff.js
+++ controllers/staff.js
@@ -7,150 +7,149 @@ const Trainer = require("../models/trainer");
 const salt = bcrypt.genSaltSync(10);
 
 exports.getAllStaff = async (req, res) => {
-    try {
-        let [[allStaff]] = await Staff.getAllStaff();
-        allStaff = await attachStaffType(allStaff);
-        res.status(200).json(allStaff);
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
-    }
+  try {
+    let [[allStaff]] = await Staff.getAllStaff();
+    allStaff = await attachStaffType(allStaff);
+    res.status(200).json(allStaff);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.getStaff = async (req, res) => {
-    try {
-        const staffId = req.params["staffId"];
-        const [[staff]] = await Staff.getStaff(staffId);
-        if (staff.length === 0) {
-            res.status(401).json({ msg: "Staff does not exist" });
-        } else {
-            const detailsToSend = staff[0];
-            delete detailsToSend["password"];
-            detailsToSend["type"] = await getStaffType(
-                detailsToSend["staff_id"]
-            );
-            res.json(detailsToSend);
-        }
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+  try {
+    const staffId = req.params["staffId"];
+    const [[staff]] = await Staff.getStaff(staffId);
+    if (staff.length === 0) {
+      res.status(401).json({ msg: "Staff does not exist" });
+    } else {
+      const detailsToSend = staff[0];
+      delete detailsToSend["password"];
+      detailsToSend["type"] = await getStaffType(detailsToSend["staff_id"]);
+      res.json(detailsToSend);
     }
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.authenticate = async (req, res) => {
-    try {
-        const username = req.body.username;
-        const password = req.body.password;
+  try {
+    console.log(1);
+    const username = req.body.username;
+    const password = req.body.password;
 
-        const [[staff]] = await Staff.getStaff(username);
-        if (staff.length == 0) {
-            res.status(404).json({
-                msg: `Staff does not exist`,
-            });
+    const [[staff]] = await Staff.getStaff(username);
+    if (staff.length == 0) {
+      res.status(404).json({
+        msg: `Staff does not exist`,
+      });
+    } else {
+      console.log(staff);
+      bcrypt.compare(password, staff[0].password, async (err, found) => {
+        if (!found) {
+          res.status(401).json({ msg: "Incorrect password" });
         } else {
-            bcrypt.compare(password, staff[0].password, async (err, found) => {
-                if (!found) {
-                    res.status(401).json({ msg: "Incorrect password" });
-                } else {
-                    // jwt stuff
-                    const user = {
-                        username,
-                        type: await getStaffType(staff[0]["staff_id"]),
-                    };
-                    const accessToken = jwt.sign(
-                        user,
-                        process.env.ACCESS_TOKEN_SECRET,
-                        { expiresIn: "7d" }
-                    );
-                    res.status(200).json({ accessToken });
-                }
-            });
+          // jwt stuff
+          const user = {
+            username,
+            type: await getStaffType(staff[0]["staff_id"]),
+            // send gym id as well
+          };
+          const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "7d",
+          });
+          res.status(200).json({ ...staff[0], accessToken });
         }
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+      });
     }
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.addStaff = async (req, res) => {
-    try {
-        const details = {
-            staffId: req.body.staffId,
-            name: req.body.name,
-            phone: req.body.phone,
-            partTime:
-                req.body.partTime == "True" || req.body.partTime == "true"
-                    ? true
-                    : false,
-            salary: req.body.salary,
-            description: req.body.description,
-            password: bcrypt.hashSync(req.body.password, salt),
-            gymId: req.body.gymId,
-        };
-        const [[staff]] = await Staff.getStaff(details.staffId);
-        if (staff.length === 0) {
-            await Staff.addStaff(details);
-            res.status(200).json({ msg: "Success" });
-        } else {
-            res.status(409).json({
-                msg: "This staff id is already registered",
-            });
-        }
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+  try {
+    const details = {
+      staffId: req.body.staffId,
+      name: req.body.name,
+      phone: req.body.phone,
+      partTime:
+        req.body.partTime == "True" || req.body.partTime == "true"
+          ? true
+          : false,
+      salary: req.body.salary,
+      description: req.body.description,
+      password: bcrypt.hashSync(req.body.password, salt),
+      gymId: req.body.gymId,
+    };
+    const [[staff]] = await Staff.getStaff(details.staffId);
+    if (staff.length === 0) {
+      await Staff.addStaff(details);
+      res.status(200).json({ msg: "Success" });
+    } else {
+      res.status(409).json({
+        msg: "This staff id is already registered",
+      });
     }
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.updateStaff = async (req, res) => {
-    try {
-        const details = {
-            staffId: req.params["staffId"],
-            name: req.body.name,
-            phone: req.body.phone,
-            partTime: req.body.partTime == 1 ? true : false,
-            salary: req.body.salary,
-            description: req.body.description,
-        };
+  try {
+    const details = {
+      staffId: req.params["staffId"],
+      name: req.body.name,
+      phone: req.body.phone,
+      partTime: req.body.partTime == 1 ? true : false,
+      salary: req.body.salary,
+      description: req.body.description,
+    };
 
-        if (!req.body.password) {
-            const [[staff]] = await Staff.getStaff(details.staffId);
-            details.password = staff[0].password;
-        } else {
-            details.password = bcrypt.hashSync(req.body.password, salt);
-        }
-        await Staff.updateStaff(details);
-        res.status(200).json({ msg: "Success" });
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+    if (!req.body.password) {
+      const [[staff]] = await Staff.getStaff(details.staffId);
+      details.password = staff[0].password;
+    } else {
+      details.password = bcrypt.hashSync(req.body.password, salt);
     }
+    await Staff.updateStaff(details);
+    res.status(200).json({ msg: "Success" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.deleteStaff = async (req, res) => {
-    try {
-        const staffId = req.params["staffId"];
-        await Staff.deleteStaff(staffId);
-        res.status(200).json({ msg: "Success" });
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
-    }
+  try {
+    const staffId = req.params["staffId"];
+    await Staff.deleteStaff(staffId);
+    res.status(200).json({ msg: "Success" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 const attachStaffType = (allStaff) => {
-    const promises = allStaff.map(async (staff) => {
-        delete staff["password"];
-        staff["type"] = await getStaffType(staff["staff_id"]);
-        return staff;
-    });
-    return Promise.all(promises);
+  const promises = allStaff.map(async (staff) => {
+    delete staff["password"];
+    staff["type"] = await getStaffType(staff["staff_id"]);
+    return staff;
+  });
+  return Promise.all(promises);
 };
 
 const getStaffType = async (staffId) => {
-    let [[details]] = await Trainer.getTrainer(staffId);
+  let [[details]] = await Trainer.getTrainer(staffId);
 
+  if (details.length != 0) {
+    return "trainer";
+  } else {
+    [[details]] = await Admin.getAdmin(staffId);
     if (details.length != 0) {
-        return "trainer";
-    } else {
-        [[details]] = await Admin.getAdmin(staffId);
-        if (details.length != 0) {
-            return "admin";
-        }
-        return "staff";
+      return "admin";
     }
+    return "staff";
+  }
 };
--- controllers/user.js
+++ controllers/user.js
@@ -7,105 +7,107 @@ const bcrypt = require("bcryptjs");
 const salt = bcrypt.genSaltSync(10);
 
 exports.getAllUsers = async (req, res) => {
-    try {
-        const [[allUsers]] = await User.getAllUsers();
-        allUsers.map((user) => delete user["password"]);
-        res.status(200).json(allUsers);
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
-    }
+  try {
+    const [[allUsers]] = await User.getAllUsers();
+    allUsers.map((user) => delete user["password"]);
+    res.status(200).json(allUsers);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.getUser = async (req, res) => {
-    try {
-        const email = req.params["email"];
-        const [[user]] = await User.getUser(email);
-        if (user.length === 0) {
-            res.status(401).json({ msg: "User does not exist" });
-        } else {
-            const detailsToSend = user[0];
-            delete detailsToSend["password"];
-            detailsToSend["type"] = "member";
-            res.json(detailsToSend);
-        }
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+  try {
+    const email = req.params["email"];
+    const [[user]] = await User.getUser(email);
+    if (user.length === 0) {
+      res.status(401).json({ msg: "User does not exist" });
+    } else {
+      const detailsToSend = user[0];
+      delete detailsToSend["password"];
+      detailsToSend["type"] = "member";
+      res.json(detailsToSend);
     }
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.authenticate = async (req, res) => {
-    try {
-        const username = req.body.username;
-        const password = req.body.password;
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+    // console.log("1");
+    const [[user]] = await User.getUser(username);
+    console.log("2");
+    if (user.length == 0) {
+      //   console.log(3);
+      res.status(404).json({
+        msg: `User does not exist`,
+      });
+    } else {
+      //   console.log(4);
 
-        const [[user]] = await User.getUser(username);
-        if (user.length == 0) {
-            res.status(404).json({
-                msg: `User does not exist`,
-            });
+      bcrypt.compare(password, user[0].password, (err, found) => {
+        if (!found) {
+          res.status(401).json({ msg: "Incorrect password" });
         } else {
-            bcrypt.compare(password, user[0].password, (err, found) => {
-                if (!found) {
-                    res.status(401).json({ msg: "Incorrect password" });
-                } else {
-                    // jwt stuff
-                    const user = { username, type: "member" };
-                    const accessToken = jwt.sign(
-                        user,
-                        process.env.ACCESS_TOKEN_SECRET,
-                        { expiresIn: "7d" }
-                    );
-                    res.status(200).json({ accessToken });
-                }
-            });
+          // jwt stuff
+          const user = { username, type: "member" };
+          const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "7d",
+          });
+          res.status(200).json({ accessToken });
         }
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+      });
     }
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.addUser = async (req, res) => {
-    try {
-        const details = {
-            email: req.body.email,
-            name: req.body.name,
-            phone: req.body.phone,
-            password: bcrypt.hashSync(req.body.password, salt),
-            dob: req.body.dob,
-            sex: req.body.sex,
-            gymId: req.body.gymId,
-        };
-        const [[user]] = await User.getUser(details.email);
-        if (user.length === 0) {
-            await User.addUser(details);
-            res.status(200).json({ msg: "Success" });
-        } else {
-            res.status(409).json({
-                msg: "User with this email already exists",
-            });
-        }
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+  try {
+    const details = {
+      email: req.body.email,
+      name: req.body.name,
+      phone: req.body.phone,
+      password: bcrypt.hashSync(req.body.password, salt),
+      dob: req.body.dob,
+      sex: req.body.sex,
+      gymId: req.body.gymId,
+    };
+    const [[user]] = await User.getUser(details.email);
+    if (user.length === 0) {
+      await User.addUser(details);
+      res.status(200).json({ msg: "Success" });
+    } else {
+      res.status(409).json({
+        msg: "User with this email already exists",
+      });
     }
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.generateResetToken = async (req, res) => {
-    try {
-        const email = req.body.email;
-        const [[user]] = await User.getUser(email);
-        if (user.length === 0) {
-            res.status(404).json({
-                msg: "User does not exist",
-            });
-        } else {
-            const token = await ResetToken.addResetToken(email);
+  try {
+    const email = req.body.email;
+    const [[user]] = await User.getUser(email);
+    if (user.length === 0) {
+      res.status(404).json({
+        msg: "User does not exist",
+      });
+    } else {
+      const token = await ResetToken.addResetToken(email);
 
-            // send email to user with link to reset password
-            const mailOptions = {
-                from: process.env.SENDER_EMAIL,
-                to: email,
-                subject: "Reset password for Absolute Fitness account",
-                html: `Dear client, 
+      // send email to user with link to reset password
+      const mailOptions = {
+        from: process.env.SENDER_EMAIL,
+        to: email,
+        subject: "Reset password for Absolute Fitness account",
+        html: `Dear client, 
                        <br /><br />
                        To reset your password, click 
                        <a href="${process.env.CLIENT_URL}/user/${email}/resetPassword/${token}">here</a>
@@ -114,82 +116,82 @@ exports.generateResetToken = async (req, res) => {
                        <br /><br />
                        Regards,<br />
                        Absolute Fitness Team.`,
-            };
+      };
 
-            transporter.sendMail(mailOptions, (err, info) => {
-                if (err) throw err;
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) throw err;
 
-                res.status(200).json({
-                    msg: "Check email for link to set new password",
-                });
-            });
-        }
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+        res.status(200).json({
+          msg: "Check email for link to set new password",
+        });
+      });
     }
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.resetPassword = async (req, res) => {
-    try {
-        const { email, token } = req.params;
-        const password = req.body.password;
-        const details = {
-            email,
-            token,
-            password: bcrypt.hashSync(password, salt),
-        };
-        await User.resetPassword(details);
-        await ResetToken.deleteOldResetTokens(email);
-        res.status(200).json({ msg: "Success" });
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
-    }
+  try {
+    const { email, token } = req.params;
+    const password = req.body.password;
+    const details = {
+      email,
+      token,
+      password: bcrypt.hashSync(password, salt),
+    };
+    await User.resetPassword(details);
+    await ResetToken.deleteOldResetTokens(email);
+    res.status(200).json({ msg: "Success" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.updateUser = async (req, res) => {
-    try {
-        const details = {
-            email: req.params["email"],
-            name: req.body.name,
-            phone: req.body.phone,
-            dob: req.body.dob.substring(0, 10),
-            sex: req.body.sex,
-            gymId: req.body.gymId,
-        };
-        if (!req.body.password) {
-            const [[user]] = await User.getUser(details.email);
-            details.password = user[0].password;
-        } else {
-            details.password = bcrypt.hashSync(req.body.password, salt);
-        }
-
-        await User.updateUser(details);
-        res.status(200).json({ msg: "Success" });
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+  try {
+    const details = {
+      email: req.params["email"],
+      name: req.body.name,
+      phone: req.body.phone,
+      dob: req.body.dob.substring(0, 10),
+      sex: req.body.sex,
+      gymId: req.body.gymId,
+    };
+    if (!req.body.password) {
+      const [[user]] = await User.getUser(details.email);
+      details.password = user[0].password;
+    } else {
+      details.password = bcrypt.hashSync(req.body.password, salt);
     }
+
+    await User.updateUser(details);
+    res.status(200).json({ msg: "Success" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.deleteUser = async (req, res) => {
-    try {
-        const email = req.params["email"];
-        await User.deleteUser(email);
-        res.status(200).json({ msg: "Success" });
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
-    }
+  try {
+    const email = req.params["email"];
+    await User.deleteUser(email);
+    res.status(200).json({ msg: "Success" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 exports.isValidResetToken = async (req, res) => {
-    try {
-        const details = { token: req.body.token, email: req.body.email };
+  try {
+    const details = { token: req.body.token, email: req.body.email };
 
-        const isValidResetToken = await ResetToken.isValidResetToken(details);
-        if (!isValidResetToken) {
-            res.status(404).json({ msg: "Invalid token" });
-        } else {
-            res.status(200).json({ msg: "Valid token" });
-        }
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
+    const isValidResetToken = await ResetToken.isValidResetToken(details);
+    if (!isValidResetToken) {
+      res.status(404).json({ msg: "Invalid token" });
+    } else {
+      res.status(200).json({ msg: "Valid token" });
     }
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
